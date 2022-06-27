4c75501dacdc8bbfcbff50aa8e547cbe9b431bb4
Iván Fretes <vanfretes@gmail.com>
2022-06-27 00:45:58 -0400

    initial commit

---
+++ src/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Juarismi\Auth;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Juarismi\Utils\Http\Middleware\Cors;
+
+class AuthServiceProvider extends ServiceProvider
+{
+
+	public function boot(){
+        $this->app->singleton('cors', function ($app) {
+            return new Cors();
+        });
+
+	    $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+	   	$this->registerRoutes();
+    }
+
+    public function register(){
+    	
+    }
+
+    protected function registerRoutes(){
+    	Route::group(['prefix' => 'api/v1'],function(){
+    		$this->loadRoutesFrom(__DIR__.'/Routes/api.php', 'jAuth');
+    	});
+    }
+
+}
---
+++ src/Routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::name('jAuth.')
+	->namespace('Juarismi\Auth\Http\Controllers')
+	->middleware(['api', 'cors'])
+	->prefix('auth')
+	->group(function ($router) {
+    
+    Route::post('login', 'JwtAuthController@login')->name('login');
+    Route::post('logout', 'JwtAuthController@logout')->name('logout');
+    Route::post('refresh', 'JwtAuthController@refresh')->name('refresh');
+    Route::post('me', 'JwtAuthController@me')->name('me');
+});
