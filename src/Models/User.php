86dc5c62e9c1c5175f41b0648782a135e3e265f6
Iván Fretes <vanfretes@gmail.com>
2023-01-18 18:08:35 -0300

    feat: change jwt package provider because the other is not compatible to php 82

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,7 +5,7 @@ namespace Juarismi\Auth\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Traits\HasRoles;
