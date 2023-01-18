ae973feaaf342401d7e19d4282447a98fb1d92b6
Iván Fretes <vanfretes@gmail.com>
2023-01-18 19:43:11 -0300

    fix: migrate

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -5,13 +5,15 @@ namespace Juarismi\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Juarismi\Utils\Http\Middleware\Cors;
+
 class AuthServiceProvider extends ServiceProvider
 {
 
 	public function boot(){
-        $this->app->singleton('cors', function ($app) {
-            return new Cors();
-        });
+      $this->app->singleton('cors', function ($app) {
+         return new Cors();
+      });
+      Schema::defaultStringLength(191);
 
 	   $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 	   $this->registerRoutes();
