e82198a50aa0b84afc0de935452a5291ef9f70b9
Iván Fretes <vanfretes@gmail.com>
2023-01-18 19:28:11 -0300

    fix: unnecesary controller

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -5,7 +5,6 @@ namespace Juarismi\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Juarismi\Utils\Http\Middleware\Cors;
-
 class AuthServiceProvider extends ServiceProvider
 {
 
@@ -14,18 +13,17 @@ class AuthServiceProvider extends ServiceProvider
             return new Cors();
         });
 
-	    $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
-	   	$this->registerRoutes();
-    }
+	   $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+	   $this->registerRoutes();
+   }
 
-    public function register(){
-    	
-    }
+   public function register(){
+   
+   }
 
-    protected function registerRoutes(){
-    	Route::group(['prefix' => 'api/v1'],function(){
+   protected function registerRoutes(){
+      Route::group(['prefix' => 'api/v1'],function(){
     		$this->loadRoutesFrom(__DIR__.'/Routes/api.php', 'jAuth');
     	});
-    }
-
+   }
 }
