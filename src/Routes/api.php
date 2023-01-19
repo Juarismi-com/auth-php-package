4f0fdf3a227897593206d084f6448354b2d9c54c
Iván Fretes <vanfretes@gmail.com>
2023-01-19 20:54:13 -0300

    feat: name of default router

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -5,9 +5,13 @@ Route::name('jAuth.')
 	->middleware(['api', 'cors'])
 	->prefix('auth')
 	->group(function ($router) {
-    
-    Route::post('login', 'JwtAuthController@login')->name('login');
-    Route::post('logout', 'JwtAuthController@logout')->name('logout');
-    Route::post('refresh', 'JwtAuthController@refresh')->name('refresh');
-    Route::post('me', 'JwtAuthController@me')->name('me');
+   
+   Route::post('login', function(){
+      return 'juarismi/auth';
+   });
+   
+   Route::post('login', 'JwtAuthController@login')->name('login');
+   Route::post('logout', 'JwtAuthController@logout')->name('logout');
+   Route::post('refresh', 'JwtAuthController@refresh')->name('refresh');
+   Route::post('me', 'JwtAuthController@me')->name('me');
 });
