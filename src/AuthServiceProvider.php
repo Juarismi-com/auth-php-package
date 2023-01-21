5b426e9c3aef5211eef140d71bf47504d402ab18
Iván Fretes <vanfretes@gmail.com>
2023-01-20 23:42:21 -0300

    fix: remove unnecesary schema in service

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -5,7 +5,6 @@ namespace Juarismi\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Juarismi\Utils\Http\Middleware\Cors;
-use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,7 +13,6 @@ class AuthServiceProvider extends ServiceProvider
       $this->app->singleton('cors', function ($app) {
          return new Cors();
       });
-      Schema::defaultStringLength(191);
 
 	   $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
 	   $this->registerRoutes();
